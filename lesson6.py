2c63bb5f67cf47974a81d9abea5cb10128d0cfc2
l-q-d <90613365+l-q-d@users.noreply.github.com>
2025-01-31 10:49:02 +0500

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,38 @@
+user_password = input("Введите пароль: ")
+password_list = list(user_password)
+score = 0
+
+
+def is_very_long(user_password):
+    return len(user_password) > 12
+
+
+def has_digit(user_password):
+    return any(char.isdigit() for char in user_password)
+
+
+def has_upper_letters(user_password):
+    return any(char.isupper() for char in user_password)
+
+
+def has_lower_letters(user_password):
+    return any(char.islower() for char in user_password)
+
+
+def has_symbols(user_password):
+    return any(not char.isdigit() and not char.isalpha() for char in user_password)
+
+
+all_functions = [
+    is_very_long,
+    has_digit,
+    has_upper_letters,
+    has_lower_letters,
+    has_symbols,
+]
+
+for check in all_functions:
+    if check(user_password):
+        score += 2
+
+print("Рейтинг пароля:", score)
